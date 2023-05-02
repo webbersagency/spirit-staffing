c991e58cc45aaeffb53787404d321d68abf99723
Pepijn van Vlaanderen <pepijn@webbers.com>
2023-05-02 09:49:02 +0200

    Fixes for animations

--- apps/web/src/utils/animations/slideInRight.ts
+++ apps/web/src/utils/animations/slideInRight.ts
@@ -1,7 +1,7 @@
 const slideInRight = (inView: boolean) => ({
     'ease-out duration-500 transition-all transform-gpu': true,
-    'opacity-0 translate-x-[40px]': !inView,
-    'opacity-1 translate-x-0': inView,
+    'opacity-5 translate-x-[40px]': !inView,
+    'opacity-100 translate-x-0': inView,
 })
 
 export default slideInRight
